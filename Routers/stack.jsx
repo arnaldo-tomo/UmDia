ad74491362e09e3b9db80b195db0f454f0149041
Arnaldo tomo <arnaldotomo@gmail.com>
2023-09-30 15:14:21 +0200

    Update stack.jsx

--- Routers/stack.jsx
+++ Routers/stack.jsx
@@ -5,19 +5,23 @@ import login from '../screens/auth/login';
 import Register from "../screens/auth/register";
 import ForgetPassword from "../screens/auth/forget";
 import Inicio from '../screens/inicio';
+import Tabs from './tabs';
+import { PaperProvider } from 'react-native-paper';
 
 const Stack = createNativeStackNavigator();
 export default function StackNavigation() {
 
     return (
-        <NavigationContainer >
-            <Stack.Navigator>
-                <Stack.Screen name="LoginScreen" component={login} options={{ headerShown: false }} />
-                <Stack.Screen name="ForgetPassword" component={ForgetPassword} options={{ headerShown: false }} />
-                <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
-                <Stack.Screen name="Inicio" component={Inicio} options={{ headerShown: false }} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <PaperProvider>
+            <NavigationContainer >
+                <Stack.Navigator>
+                    <Stack.Screen name="LoginScreen" component={login} options={{ headerShown: false }} />
+                    <Stack.Screen name="ForgetPassword" component={ForgetPassword} options={{ headerShown: false }} />
+                    <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
+                    <Stack.Screen name="Inicio" component={Tabs} options={{ headerShown: false }} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </PaperProvider>
     );
 }
 
