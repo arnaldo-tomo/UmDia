fefddbbd0642a0be1988f84bf6890347358d9960
Arnaldo tomo <arnaldotomo@gmail.com>
2023-09-30 11:50:08 +0200

    Create index.jsx

---
+++ config/index.jsx
@@ -0,0 +1,13 @@
+export const PRIMARY_COLOR = "#0095f6";
+export const SECUNDARY_COLOR = "#F9F9F9";
+export const PADRAO_COLOR = "#FFF";
+
+export const MT = 4;
+export const MB = 4;
+
+export const WIGTH = "90%";
+export const HEIGHT = "40%";
+
+export const FONT_SIZE = 16;
+export const LINE_HEIGHT = 20;
+export const FONT_WEIGHT = "bold";
