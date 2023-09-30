58916e365ab7e63bf90c5e13fb398f7ae26d83c2
Arnaldo tomo <arnaldotomo@gmail.com>
2023-09-30 15:14:24 +0200

    Update tabs.jsx

--- Routers/tabs.jsx
+++ Routers/tabs.jsx
@@ -1,33 +1,86 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Text, BottomNavigation, PaperProvider } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const Tab = createBottomTabNavigator();
+
+export default function Tabs({ navigation }) {
+    return (
+
+        <Tab.Navigator
+            initialRouteName="Feed"
+            activeColor="#e91e63"
+            barStyle={{ backgroundColor: 'tomato' }}
+        >
+            <Tab.Screen
+                name="Feed"
+                component={HomeScreen}
+                options={{
+                    headerShown: false,
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="home" color={color} size={26} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Notifications"
+                component={SettingsScreen}
+                options={{
+                    headerShown: false,
+                    tabBarLabel: 'Updates',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="bell" color={color} size={26} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={Profile}
+                options={{
+                    headerShown: false,
+                    tabBarLabel: 'Profile',
+                    tabBarIcon: ({ color }) => (
+                        <MaterialCommunityIcons name="account" color={color} size={26} />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+
+
+    );
+}
 
 function HomeScreen() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Home!</Text>
+        <View style={styles.container}>
+            <Text variant="headlineMedium">Home!</Text>
         </View>
     );
 }
 
 function SettingsScreen() {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Settings!</Text>
+        <View style={styles.container}>
+            <Text variant="headlineMedium">Settings!</Text>
+        </View>
+    );
+}
+function Profile() {
+    return (
+        <View style={styles.container}>
+            <Text variant="headlineMedium">Settings!</Text>
         </View>
     );
 }
 
-const Tab = createBottomTabNavigator();
-
-export default function Tbas() {
-    return (
-        <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeScreen} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
-}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
