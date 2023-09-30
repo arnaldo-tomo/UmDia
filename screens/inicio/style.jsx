76a2eadc0f4eaaf18d062b951d4cb65ac47e3c32
Arnaldo tomo <arnaldotomo@gmail.com>
2023-09-30 11:50:55 +0200

    Create style.jsx

---
+++ screens/inicio/style.jsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const InicioStyle = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F5FCFF"
+    }
+})
