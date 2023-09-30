56b2a4eb31a2da7fd9c25aca5dc2764761a9b439
Arnaldo tomo <arnaldotomo@gmail.com>
2023-09-30 11:49:36 +0200

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,11 @@
+
+import StackNavigation from "./Routers/stack";
+import Tbas from "./Routers/tabs";
+export default function App() {
+  return (
+    <StackNavigation />
+    // <Tbas />
+  );
+}
+
+
