7e1872ecbde53de5031e873b886aa62f093dfdf3
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-17 15:31:53 +0200

    Create themas.jsx

---
+++ config/themas.jsx
@@ -0,0 +1,13 @@
+export const PRIMARY_COLOR = "#ADF547";
+export const SECUNDARY_COLOR = "#1F212C";
+export const LIGHT = "#FFFFFF";
+export const SPANCING = 10;
+export const BORDER = "#504f4f";
+export const BLACK = "#000000";
+
+const white = "#FFFFFF";
+const gray = "#504f4f";
+const black = "#000000";
+const dark = "#1F212C";
+const green = "#ADF547";
+const yellow = "#F2E205";
