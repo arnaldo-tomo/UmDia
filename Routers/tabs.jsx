4efccfa63f8a7bfb3e5d4beef68222f3873ee50d
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-17 15:32:23 +0200

    Update tabs.jsx

--- Routers/tabs.jsx
+++ Routers/tabs.jsx
@@ -1,75 +1,125 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Text, BottomNavigation, PaperProvider } from 'react-native-paper';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Text, BottomNavigation } from 'react-native-paper';
+import { CommonActions } from "@react-navigation/native";
+
+// import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Ionicons } from "@expo/vector-icons";
+
+import { HomePage } from '../screens/home';
+import { BLACK, LIGHT } from '../config/themas';
 
 const Tab = createBottomTabNavigator();
 
-export default function Tabs({ navigation }) {
+export default function Tabs() {
     return (
-
         <Tab.Navigator
-            initialRouteName="Feed"
-            activeColor="#e91e63"
-            barStyle={{ backgroundColor: 'tomato' }}
+            screenOptions={{ headerShown: false, }}
+            tabBar={({ navigation, state, descriptors, insets }) => (
+                <BottomNavigation.Bar
+                    style={{ backgroundColor: '#1F212C' }}
+                    navigationState={state}
+                    safeAreaInsets={insets}
+                    onTabPress={({ route, preventDefault }) => {
+                        const event = navigation.emit({
+                            type: 'tabPress',
+                            target: route.key,
+                            canPreventDefault: true,
+                        });
+
+                        if (event.defaultPrevented) {
+                            preventDefault();
+                        } else {
+                            navigation.dispatch({
+                                ...CommonActions.navigate(route.name, route.params),
+                                target: state.key,
+                            });
+                        }
+                    }}
+                    renderIcon={({ route, focused, color }) => {
+                        const { options } = descriptors[route.key];
+                        if (options.tabBarIcon) {
+                            return options.tabBarIcon({ focused, color, size: 24 });
+                        }
+
+                        return null;
+                    }}
+                    getLabelText={({ route }) => {
+                        const { options } = descriptors[route.key];
+                        const label =
+                            options.tabBarLabel !== undefined
+                                ? options.tabBarLabel
+                                : options.title !== undefined
+                                    ? options.title
+                                    : route.title;
+
+                        return label;
+                    }}
+
+                />
+            )}
         >
             <Tab.Screen
-                name="Feed"
-                component={HomeScreen}
+                name="Home"
+                component={HomePage}
                 options={{
-                    headerShown: false,
-                    tabBarLabel: 'Home',
-                    tabBarIcon: ({ color }) => (
-                        <MaterialCommunityIcons name="home" color={color} size={26} />
-                    ),
+                    tabBarLabel: 'Home', tabBarInactiveBackgroundColor: "blue",
+                    tabBarIcon: ({ color, size }) => {
+                        return <Ionicons name="home-outline" size={size} color={BLACK} />;
+
+                    },
                 }}
             />
             <Tab.Screen
-                name="Notifications"
+                name="Users"
+                component={Users}
+                options={{
+                    tabBarIcon: ({ color, size }) => {
+                        return <Ionicons name="people-outline" size={size} color={LIGHT} />;
+                    },
+                }}
+            />
+            <Tab.Screen
+                name="Calendar"
+                component={Calendar}
+                options={{
+                    tabBarIcon: ({ color, size }) => {
+                        return <Ionicons name="calendar-outline" size={size} color={LIGHT} />;
+                    },
+                }}
+            />
+            <Tab.Screen
+                name="SettingsScreen"
                 component={SettingsScreen}
                 options={{
-                    headerShown: false,
-                    tabBarLabel: 'Updates',
-                    tabBarIcon: ({ color }) => (
-                        <MaterialCommunityIcons name="bell" color={color} size={26} />
-                    ),
-                }}
-            />
-            <Tab.Screen
-                name="Profile"
-                component={Profile}
-                options={{
-                    headerShown: false,
-                    tabBarLabel: 'Profile',
-                    tabBarIcon: ({ color }) => (
-                        <MaterialCommunityIcons name="account" color={color} size={26} />
-                    ),
+                    tabBarIcon: ({ color, size }) => {
+                        return <Ionicons name="person-outline" size={size} color={LIGHT} />;
+                    },
                 }}
             />
         </Tab.Navigator>
-
-
     );
 }
 
-function HomeScreen() {
-    return (
-        <View style={styles.container}>
-            <Text variant="headlineMedium">Home!</Text>
-        </View>
-    );
-}
 
-function SettingsScreen() {
+
+function Calendar() {
     return (
         <View style={styles.container}>
             <Text variant="headlineMedium">Settings!</Text>
         </View>
     );
 }
-function Profile() {
+function Users() {
+    return (
+        <View style={styles.container}>
+            <Text variant="headlineMedium">Settings!</Text>
+        </View>
+    );
+}
+function SettingsScreen() {
     return (
         <View style={styles.container}>
             <Text variant="headlineMedium">Settings!</Text>
