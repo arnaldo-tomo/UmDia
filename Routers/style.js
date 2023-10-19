d4d87bc10f05480e963ecd04f98ec62e0cefe756
Arnaldo tomo <arnaldotomo@gmail.com>
2023-10-19 15:28:11 +0200

    Update style.js

--- Routers/style.js
+++ Routers/style.js
@@ -1,5 +1,19 @@
+import { Dimensions } from "react-native";
+import { StyleSheet } from "react-native";
+import { PRIMARY_COLOR, SPANCING } from "../config/themas";
+const { width } = Dimensions.get('screen')
 export const style = StyleSheet.create({
     tabBar: {
-
+        backgroundColor: '#1F212C',
+        color: "#FFF",
+        bottom: 20,
+        borderRadius: 20,
+        marginRight: 10,
+        marginLeft: 10,
+        overflow: 'hidden',
+        position: 'absolute'
+    }, home: {
+        textDecorationColor: 'red',
+        color: 'red',
     }
 })
