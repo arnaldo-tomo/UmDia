2456d6372af2f66749192530649d5969e6a4e5fc
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-12 20:16:24 +0200

    Update stack.jsx

--- Routers/stack.jsx
+++ Routers/stack.jsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import login from '../screens/auth/login';
-import Register from "../screens/auth/register";
-import ForgetPassword from "../screens/auth/forget";
 import { Onboarding } from '../screens/onboarding';
 import Tabs from './tabs';
 import { OverView } from '../screens/overview';
@@ -18,9 +15,6 @@ export default function StackNavigation() {
                 <Stack.Screen name="Boarding" component={Onboarding} options={{ headerShown: false }} />
                 <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false }} />
                 <Stack.Screen name="OverView" component={OverView} options={{ headerShown: false }} />
-                <Stack.Screen name="login" component={login} options={{ headerShown: false }} />
-                <Stack.Screen name="ForgetPassword" component={ForgetPassword} options={{ headerShown: false }} />
-                <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
             </Stack.Navigator>
         </NavigationContainer>
     );
